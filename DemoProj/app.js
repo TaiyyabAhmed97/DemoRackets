0c8fba2278ab720e7950ea635d34b9b7a823bb12
Taiyyab Ahmed <hafiztaiyyab11@gmail.com>
2017-12-08 23:30:42 -0600

    connecting to db, currently not working

--- DemoProj/app.js
+++ DemoProj/app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -13,6 +14,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+mongoose.connect('mongodb://localhost/Strings');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
