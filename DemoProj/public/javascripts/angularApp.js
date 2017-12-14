d936a1557cf7e7a4e19d2cf5aec742e0f932e9bb
Paul Botti <pbotti@hawk.iit.edu>
2017-12-13 19:16:40 -0600

    It's a bit ugly but it's getting somewhere

--- DemoProj/public/javascripts/angularApp.js
+++ DemoProj/public/javascripts/angularApp.js
@@ -1,14 +1,13 @@
 var app = angular.module('StringsAttached', []);
+//var mongoose = require('mongoose');
+//var db = mongoose.createConnection('mongodb://localhost:27017/database');
 
 app.controller('MainCtrl', [
-'$scope',
-function($scope){
-  $scope.test = 'Hello world!';
-  $scope.posts = [
-    'post 1',
-    'post 2',
-    'post 3',
-    'post 4',
-    'post 5'
-  ];
+'$scope', '$http', 
+function ($scope, $http) {
+    $scope.test = "hello world!";
+    $http.get('/demos').then(function (response) {
+        $scope.demos = response.data;
+        //console.log("demos", $scope.demos);
+    });
 }]);
