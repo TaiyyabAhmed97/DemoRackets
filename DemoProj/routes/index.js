adbc6da9794ef905d603767c518df2256cd1bd2c
Taiyyab Ahmed <hafiztaiyyab11@gmail.com>
2018-02-03 16:50:10 -0600

    somthign went wrong something went right

--- DemoProj/routes/index.js
+++ DemoProj/routes/index.js
@@ -9,88 +9,58 @@ mongoose.connect('mongodb://localhost/test');
 var Customer = require('../models/Customer');
 var CurrDemo = require('../models/CurrentDemo');
 //below is a test route
-router.get('/', function(req, res, next){
 
-  res.json("Hi");
-});
 
-//above is a test route
-router.get('/nopost',function(req, res, next){
-  console.log("here");
-  Customer.find({}).remove().exec();
-  CurrDemo.find({}).remove().exec();
-  res.json('works');
 
-});
 
-router.delete('/:id', function(req, res, next){
-  Customer.findByIdAndRemove(req.params.id, req.body, function(err, post){
-    if(err) return next(err);
-    res.json(post);
-
+/* GET ALL CustomerS */
+router.get('/', function(req, res, next) {
+  Customer.find(function (err, products) {
+    if (err) return next(err);
+    res.json(products);
   });
 });
 
-router.get('/customers', function(req, res, next){
-  console.log("not here");
-  Customer.find({}).exec(function(err, ret){
-    res.json(ret);
-  });
-}); // GET all Customers
-
-router.post('/something', function(req, res, next){
-Customer.create(req.body, function (err, post){
-  if (err) return next(err);
+/* GET SINGLE Customer BY ID */
+router.get('/:id', function(req, res, next) {
+  Customer.findById(req.params.id, function (err, post) {
+    if (err) return next(err);
     res.json(post);
+  });
 });
-var client = new twilio(accountSid, authToken);
+
+/* SAVE Customer */
+router.post('/', function(req, res, next) {
+  var client = new twilio(accountSid, authToken);
   client.messages.create({
 	body: 'Hello '+req.body.firstname+'We hope you have fun with your new racket',
-	to: '+17737128894',
+	to: '+1'+req.body.PhoneNum,
 	from: '+12242493721'
 })
 .then((message) => console.log(message.sid));
-}); // POST a Customer
-
-router.get('/customer/:phonenum', function(req, res, next){
-  Customer.find({'PhoneNum': req.params.phonenum})
-    .exec(function(err, ret)
-  {
-    
-    res.json(ret);
+  Customer.create(req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
   });
-}); // PUT a customer (update name/num/email/phonenum)
+});
 
-router.delete('/customer/:id', function(req, res, next){}); //DELETE a customer
-
-router.get('/demos', function(req, res, next){
-  CurrDemo.find({})
-  .populate('Customer')
-  .populate('Racket')
-  .exec(function(err, ret){
-    res.json(ret);
+/* UPDATE Customer */
+router.put('/:id', function(req, res, next) {
+  Customer.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
   });
-}); //GET all current demo-ed out rackets
+});
 
-router.post('/demos', function(req, res, next){
-  
-  let cust1 = Customer.find({'PhoneNum': req.body.Customer.PhoneNum})
-    .exec(function(err, ret){
-      return ret;
-    });
-  let arr = JSON.parse(JSON.stringify(req.body.Rackets));
-  let date = req.body.CheckedOut;
-  let temp = new CurrDemo({Customer:cust1, CheckedOut: date, Rackets: arr});
-  temp.save();
-  
-}); // POST a (transaction)
+/* DELETE Customer */
+router.delete('/:id', function(req, res, next) {
+  Customer.findByIdAndRemove(req.params.id, req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
 
-router.put('/demos/:id', function(req, res, next){}); //PUT a transaction (update cust info or racket info or check out date)
-
-router.delete('/demos/:id', function(req, res, next){
-CurrDemo.remove(req.params.id, callback);
-
-}); //DELETE a transaction ( for now, later we can move this to another table "Returned Demos")
+module.exports = router;
 
 
 
