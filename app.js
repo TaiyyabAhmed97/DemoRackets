53f0246d65e3a7c5d62e1ba7e1accbc582b68618
Linux <hafiztaiyyab11@gmail.com>
2018-06-20 00:07:34 -0500

    added stati files for angualr

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('../rent-a-racket/dist'));
 
 app.use('/', index);
 app.use('/users', users);
