b7da26936418e9a65a46be01c5187afb51241175
Linux <hafiztaiyyab11@gmail.com>
2018-07-24 16:48:39 -0500

    must run with  ng serve --proxy-config proxy.conf.json --host 0.0.0.0

--- app.js
+++ app.js
@@ -38,11 +38,11 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 
-app.use(function (req, res, next) {
+/*app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
-});
+}); */
 
 
 app.use(function (req, res, next) {
